55a32c33e25ad1014f3374c1f3b3accc6851cf95
David Santos <praticotoday@gmail.com>
2025-04-08 14:26:33 -0300

    Add files via upload

--- html-css/testes prompt youtube/teste004/script.js
+++ html-css/testes prompt youtube/teste004/script.js
@@ -1,15 +1,37 @@
-let circulo = document.querySelector(".circulo")
-let imagem = document.querySelector(".starbucks")
+ const mario = document.querySelector('.mario');
+ const pipe = document.querySelector('.pipe');
 
+ const jump = () => {
+    mario.classList.add ('jump');
 
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+ }
 
-function trocarCor(cor){
-     circulo.style.background = cor
-}
+ const loop = setInterval (() => {
+   console.log('loop')
 
-function trocaImagem(img){
-     imagem.src = img
+   const pipePosition = pipe.offsetLeft;
+   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-}
+   console.log(marioPosition);
 
-    
+   if (pipePosition <= 120 && pipePosition > 0 &&marioPosition < 80) {
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = 'mario-jump-images/game-over.png'
+    mario.style.width = '75px'
+    mario.style.marginLeft = '50px'
+
+    clearInterval(loop);
+
+   }
+ }, 10);
+  
+ document.addEventListener ('keydown', jump);
